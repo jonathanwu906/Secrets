9c508eb98b6f23718f5095bfc8dda2a7e9f941a1
jonathanwu906 <jonathanwu906@gmail.com>
2022-09-10 22:59:56 +0800

    Add passportlocalmongoose

--- app.js
+++ app.js
@@ -1,81 +1,114 @@
-require('dotenv').config()
+//  ***** Require Section *****
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
-app.set("view engine", 'ejs');
-app.use(bodyParser.urlencoded({
-  extended: true
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
 }));
 
-mongoose.connect("mongodb://127.0.0.1/userDB")
+app.use(passport.initialize());
+app.use(passport.session());
 
+mongoose.connect("mongodb://127.0.0.1/userDB");
+// mongoose.set("useCreateIndex", true); // ==> to fix error names (DeprecationWarning)
+
+// **** Schema encrypted type ****
 const userSchema = new mongoose.Schema ({
-  email: String,
-  password: String
+    email:String,
+    password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
-
-
+// **** Model ****
 const User = new mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
 
-app.get("/", function(req, res) {
-  res.render("home");
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", function(req, res){
+    res.render("home");
 });
 
-app.get("/login", function(req, res) {
-  res.render("login");
+app.get("/login", function(req, res){
+    res.render("login");
 });
 
-app.get("/register", function(req, res) {
-  res.render("register");
+app.get("/register", function(req, res){
+    res.render("register");
 });
 
-app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
-
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
+app.get("/secrets", function(req, res){
+    if (req.isAuthenticated()){
+        res.render("secrets");
     }
-  });
+    else {
+        res.redirect("/login");
+    }
 });
 
-app.post("/login", function(req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  User.findOne({email: username}, function(err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
+// ****** We need call back function here : On /logout : *****
+app.get("/logout", function(req, res){
+    req.logout(function(err){
+        if (err) {
+            console.log(err);
         }
-      }
-    }
-  });
-
+        else {
+            res.redirect("/");
+        }
+    });
 });
 
+app.post("/register", function(req, res){
 
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        }
+        else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }
+    });
+});
 
+app.post("/login", function(req, res){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, function(err){
+        if (err) {
+            console.log(err);
+        }
+        else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }
+    });
+});
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000.");
+// ***** Port Section *****
+app.listen(3000, function(){
+    console.log("Server Started on port 3000.");
 });
